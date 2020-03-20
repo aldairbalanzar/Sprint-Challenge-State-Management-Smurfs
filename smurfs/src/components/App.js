409e3d1ffa38194a177f4ebf1605bad7059cb7e8
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-03-20 13:34:13 -0400

    updated files to what i had

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,85 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from 'react-redux';
+import { loadData, postData } from '../actions/appActions';
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
+
+
+const App = props => {
+
+  const handleLoad = e => {
+    e.preventDefault();
+    props.loadData();
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.postData({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  console.log('line 14: ', props.dataList);
+
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+
+      <button onClick={handleLoad}> Load Data </button>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          <input
+           type="text"
+           id="name"
+           name="name"
+           
+           placeholder="name"
+          />
+        </label>
+
+        <label htmlFor="age">
+          <input
+           type="text"
+           id="age"
+           name="age"
+           
+           placeholder="age"
+          />
+        </label>
+
+        <label htmlFor="height">
+          <input
+           type="text"
+           id="height"
+           name="height"
+           
+           placeholder="height"
+          />
+        </label>
+
+        <button type="submit">submit</button>
+      </form>
+
+      <section>
+        {props.dataList.map(item => {
+          return(
+            <div key={item.id}>
+              <p>name: {item.name}</p>
+              <p>age: {item.age}</p>
+              <p>height: {item.height}</p>
+            </div>
+          )
+        })}
+      </section>
+
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    dataList: state.dataList
   }
 }
 
-export default App;
+export default connect(mapStateToProps, { loadData, postData })(App);
---
+++ smurfs/src/reducers/appReducer.js
@@ -0,0 +1,36 @@
+import { FETCH_DATA, FETCH_DATA_SUCCESS, SET_ERROR, POST_DATA } from '../actions/appActions';
+
+const initialState = {
+    dataList: [],
+    isFetching: false,
+    error: ''
+}
+
+export const appReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_DATA: 
+            return {
+                ...state,
+                isFetching: true,
+                dataList: []
+            };
+            case FETCH_DATA_SUCCESS: 
+                return {
+                ...state,
+                dataList: [...state.dataList, ...action.payload],
+                isFetching: false
+            };
+            case POST_DATA:
+                return {
+                    datalist: [...state.dataList, action.payload]
+                }
+            case SET_ERROR: 
+                return {
+                    ...state,
+                    isFetching: false,
+                    error: `Error: ${action.payload}`
+                }
+        default:
+            return state
+    }
+}
